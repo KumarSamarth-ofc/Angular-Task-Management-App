4399d1d53d00590fcbe4e38bdaf249724bd431ef
KumarSamarth-ofc <kumarsamarth.adept@gmail.com>
2026-05-07 15:38:02 +0530

    Implement and Integrate logim

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    const userData=  {
+      email: this.email,
+      password: this.password,
+    }
+
+    this.authService.login(userData).subscribe({
+      next: (response: any) => {
+        console.log(response);
+
+        localStorage.setItem('token',response.token);
+        alert('Login successful!');
+        this.router.navigate(['/dashboard']);
+      },
+      
+      error: (error) => {
+        console.error('Login failed:', error);
+        alert('Login failed. Please check your credentials and try again.');
+      }
+    })
+  }
+ }
+
